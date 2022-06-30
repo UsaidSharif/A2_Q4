e6d2bd97d7e721bd2ae8124e6f68ed34ac567456
u2018344 <u2018344@giki.edu.pk>
2022-06-30 07:03:09 -0400

    Add project files.

---
+++ A2_Q4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace A2_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write Number of elements to be entered in an array!");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            int[] terms = new int[Number];
+
+            for (int i = 0; i < Number; i++)
+            {
+                Console.WriteLine("Write elements to be entered in an array!");
+                int value = Convert.ToInt32(Console.ReadLine());
+                terms[i] = value;
+            }
+
+            Console.WriteLine("Array you made is\n ");
+
+            for (int m = 0; m < Number; m++)
+            {
+                Console.Write(" " + terms[m]+"\n");
+            }
+            Console.WriteLine("New Array after removing duplicates is\n ");
+            int[] distinct = terms.Distinct().ToArray();
+            Console.WriteLine(String.Join(" ", distinct));
+
+            
+        }
+    }
+}
